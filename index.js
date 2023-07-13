8c52423d2bf801c49d9c041d36bb519ef6564c4e
Oleg Dzanaev <dzanaeffoleg@gmail.com>
2023-07-13 21:11:24 +0200

    edit1

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
 const saveButton = document.querySelector('.popup__save');
 
-const nameInput = document.querySelector('input[name="name"]');
-const nameAbout = document.querySelector('input[name="about"]');
+const nameInput = document.querySelector('.popup__form__input__name');
+const aboutInput = document.querySelector('.popup__form__input__about');
 
 function togglePopup() {
   popupAbout.classList.toggle('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  aboutInput.value = profileSubtitle.textContent;
 }
 
 profileEditButton.addEventListener('click', togglePopup);
 buttonCloseAboutPopup.addEventListener('click', togglePopup);
 
-nameInput.value = profileTitle.textContent;
-nameAbout.value = profileSubtitle.textContent;
-
-saveButton.addEventListener('click', function() {
+saveButton.addEventListener('click', function(event) {
+  event.preventDefault();
   profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = nameAbout.value;
+  profileSubtitle.textContent = aboutInput.value;
+  togglePopup();
+});
+
+const profileForm = document.querySelector('.popup__form');
+profileForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = aboutInput.value;
   togglePopup();
 });
