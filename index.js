5f76196f11a0da83c77075e572f0c94d65f6458a
Oleg Dzanaev <dzanaeffoleg@gmail.com>
2023-07-14 18:39:25 +0200

    edit 3

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const popupAbout = document.querySelector('.popup_type_edit');
 const buttonCloseAboutPopup = popupAbout.querySelector('.popup__close');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const saveButton = document.querySelector('.popup__save');
 
 const nameInput = document.querySelector('.popup__input_name');
 const aboutInput = document.querySelector('.popup__input_about');
@@ -16,13 +15,6 @@ function togglePopup() {
 
 profileEditButton.addEventListener('click', togglePopup);
 buttonCloseAboutPopup.addEventListener('click', togglePopup);
-
-saveButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = aboutInput.value;
-  togglePopup();
-});
 
 const profileForm = document.querySelector('.popup__form');
 profileForm.addEventListener('submit', function(event) {
